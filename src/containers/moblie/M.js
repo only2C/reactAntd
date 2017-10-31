5f5887ca9b21adb9503bf14a776722df46c030f1
chenli <chenliw@yonyou.com>
2017-10-31 16:03:36 +0800

    'init'

---
+++ src/containers/moblie/M.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {observer} from 'mobx-react';
+import globalStore from '../../stores/GlobalStore';
+
+@observer
+export default class M extends React.Component {
+    render(){
+        return (
+            <div>123</div>
+        )
+    }
+}
